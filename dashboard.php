86c7f5f15b3403729d793c82a4a29fc3f06e923b
earlkian8 <earlkian8@gmail.com>
2025-03-16 13:09:30 +0800

    Added Navigations

--- dashboard.php
+++ dashboard.php
@@ -4,8 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WageFlow</title>
+    <link rel="stylesheet" href="css/navigation-style.css">
 </head>
 <body>
-    
+
+    <!-- Side Bar -->
+    <aside class="aside-style">
+        <div class="container1">
+            <img src="images/logo.png" alt="Logo" class="main-logo-style">
+            <h1 class="h1-style">Wage<span class="span-style">Flow</span></h1>
+        </div>
+        <div class="container2">
+            <a href="dashboard.php" class="a-style">
+                <div class="a-container-style">
+                    <img src="/images/home-61ccef.png" alt="Home" class="logo-style">
+                    <p class="p-style" id="dashboard-underline">DASHBOARD</p> 
+                </div>
+            </a>
+            <a href="employee.php" class="a-style">
+                <div class="a-container-style">
+                    <img src="/images/user-61CCEF.png" alt="Employee" class="logo-style">
+                    <p class="p-style">EMPLOYEE</p>
+                </div>
+            </a>
+            <a href="leave_approval.php" class="a-style">
+                <div class="a-container-style">
+                    <img src="/images/leave-61ccef.png" alt="Leave" class="logo-style">
+                    <p class="p-style">LEAVE APPROVAL</p> 
+                </div>
+            </a>
+            <a href="payroll.php" class="a-style">
+                <div class="a-container-style">
+                    <img src="/images/payroll-61ccef.png" alt="Payroll" class="logo-style">
+                    <p class="p-style">PAYROLL</p>
+                </div>
+            </a>
+        </div>
+    </aside>
+
+    <!-- Content -->
+    <main class="main-style">
+    </main>
 </body>
 </html>
--- login.php
+++ login.php
@@ -11,7 +11,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>WageFlow | Sign In</title>
-        <link rel="stylesheet" href="css/login.css" />
+        <link rel="stylesheet" href="css/login-style.css" />
     </head>
     <body>
         <div class="login-container">
